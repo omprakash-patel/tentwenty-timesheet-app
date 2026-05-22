02fda8430d65747ab708fe512c9c9df15cd8f24a
Omprakash Patel <connecttoom29@gmail.com>
2026-05-22 19:19:33 +0530

    changes1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 // import Image from "next/image";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-    Home
+    <Link href="/login">Login</Link>
     </>
   );
 }
